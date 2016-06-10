82d9542a87b99d792eecb5505532fd98c318d904
Nicolas VINCENT <nico.vince@gmail.com>
2016-06-10 16:58:21 +0200

    PEP8 : fix errors

--- mche.py
+++ mche.py
@@ -4,6 +4,7 @@ import sys
 import re
 from binascii import hexlify
 
+
 class RegionFile:
     """
     Class to handle Minecraft Region file
@@ -35,10 +36,10 @@ class RegionFile:
                     data_offset = int(hexlify(cur_loc[0:3]), 16)
                     # Number of sectors (4096 bytes) occupied by chunk
                     data_count = int(hexlify(cur_loc[3]), 16)
-                    self.chunks.append({"offset" : data_offset,
-                                        "sector_count" : data_count,
-                                        "x" : x,
-                                        "z" : z})
+                    self.chunks.append({"offset": data_offset,
+                                        "sector_count": data_count,
+                                        "x": x,
+                                        "z": z})
 
             # Read timestamps (4096 bytes)
             self.timestamps_field = f.read(4096)
@@ -47,21 +48,24 @@ class RegionFile:
                     # Offset of current chunk's timestamp in timestamp field
                     offset = 4*(x + 32*z)
                     cur_ts = self.timestamps_field[offset:offset+4]
-                    self.chunks[offset/4]["timestamp"] = int(hexlify(cur_ts), 16)
+                    cur_ts = int(hexlify(cur_ts), 16)
+                    self.chunks[offset/4]["timestamp"] = cur_ts
 
             # Read chunk datas (number of data deduced from location fields
-            total_size=8192
+            total_size = 8192
             for z in range(32):
                 for x in range(32):
                     # chunk index in chunks list
                     chunk_idx = x + 32*z
-                    chunk_size = 4096 * self.chunks[chunk_idx]["sector_count"]
-                    if chunk_size > 0:
-                        total_size += chunk_size
-                        self.chunks[chunk_idx]["chunk_data"] = f.read(chunk_size)
+                    size = 4096 * self.chunks[chunk_idx]["sector_count"]
+                    if size > 0:
+                        total_size += size
+                        self.chunks[chunk_idx]["chunk_data"] = f.read(size)
 
             file_size = os.path.getsize(self.region_filename)
-            assert total_size == file_size, "Size read in file (%d) does not match file size (%d)" % (total_size, file_size)
+            assert total_size == file_size,\
+                "Size read in file (%d) does not match file size (%d)"\
+                % (total_size, file_size)
 
     def write(self, filename):
         """
@@ -70,7 +74,7 @@ class RegionFile:
         Filename can be the same as self.region_filename if you are sure of
         your changes or have made a backup earlier
         """
-        #TODO placeholder
+        # TODO placeholder
 
     def display_chunk_info(self, x, z):
         """
@@ -79,8 +83,7 @@ class RegionFile:
         Coordinates of chunks are relative coords from the region file
         """
         # Index of chunk in chunks list
-        chunk_idx = (x%32) + 32*(z%32)
-        #print self.chunks[chunk_idx]
+        chunk_idx = (x % 32) + 32*(z % 32)
         print self.chunks[chunk_idx]["offset"]
         print self.chunks[chunk_idx]["sector_count"]
         print self.chunks[chunk_idx]["x"]
@@ -102,7 +105,8 @@ class RegionFile:
         """
         Get chunk coordinates in blocks
 
-        Coordinates of chunk are given in relative coords from the region file ([0-31], [0-31])
+        x : relative chunk x-coordinate in region file [0-31]
+        z : relative chunk z-coordinate in region file [0-31]
 
         Return tuple of two points giving two opposite corners of the chunk
         ((x1, z1), (x2, z2))
@@ -115,23 +119,13 @@ class RegionFile:
         x2 = x_region_offset + x_chunk_offset + 15
         z1 = z_region_offset + z_chunk_offset
         z2 = z_region_offset + z_chunk_offset + 15
-        return ((x1, z1), (x2,z2))
+        return ((x1, z1), (x2, z2))
 
 if __name__ == "__main__":
     rf = RegionFile("/home/pi/mc/juco/region/r.3.3.mca")
     rf.read()
-    rf.display_chunk_info(0,0)
-    rf.display_chunk_info(31,31)
+    rf.display_chunk_info(0, 0)
+    rf.display_chunk_info(31, 31)
 
-    #for c in sorted(rf.chunks, key=lambda x: x["offset"]):
-    #    print c
-
-    #rf = RegionFile("/home/pi/mc/juco/region/r.0.0.mca")
-    #rf.read()
-    #rf.display_chunk_info(0,0)
-    #rf.display_chunk_info(31,31)
-
-    #rf = RegionFile("/home/pi/mc/juco/region/r.-1.-1.mca")
-    #rf.read()
-    #rf.display_chunk_info(0,0)
-    #rf.display_chunk_info(31,31)
+    # for c in sorted(rf.chunks, key=lambda x: x["offset"]):
+    #     print c
